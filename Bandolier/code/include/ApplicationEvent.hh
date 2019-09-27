bc6d915eaf631627296863d8ef874151652b39af
Chris <chris.sam55@gmail.com>
2019-09-26 21:32:09 -0400

    Added WindowEvent event base type, for more granularity

--- Bandolier/code/include/ApplicationEvent.hh
+++ Bandolier/code/include/ApplicationEvent.hh
@@ -19,7 +19,21 @@ public:
   EVENT_TYPE(EventType::App)
 };
 
-class WindowClose : public ApplicationEvent
+class WindowEvent : public ApplicationEvent
+{
+public:
+  using Trigger_t = gsw::event_trigger<ApplicationEvent>;
+  using Channel_t = Trigger_t::channel_t;
+  using Handler_t = Channel_t::handler;
+  using SimpleHandler_t = Channel_t::simple_handler;
+
+  virtual const char* Name() const {return "WindowEvent"; }
+
+  EVENT_CATEGORY(EventCategory::Application)
+  EVENT_TYPE(EventType::Window)
+};
+
+class WindowClose : public WindowEvent
 {
 public:
   using Trigger_t = gsw::event_trigger<WindowClose>;
@@ -33,7 +47,7 @@ public:
   EVENT_TYPE(EventType::WindowClose)
 };
 
-class WindowResize : public ApplicationEvent
+class WindowResize : public WindowEvent
 {
 public:
   using Trigger_t = gsw::event_trigger<WindowResize>;
@@ -70,7 +84,7 @@ public:
   EVENT_TYPE(EventType::WindowResize)
 };
 
-class WindowGainedFocus : public ApplicationEvent
+class WindowGainedFocus : public WindowEvent
 {
 public:
   using Trigger_t = gsw::event_trigger<WindowGainedFocus>;
@@ -86,7 +100,7 @@ public:
   EVENT_TYPE(EventType::WindowFocus)
 };
 
-class WindowLostFocus : public ApplicationEvent
+class WindowLostFocus : public WindowEvent
 {
 public:
   using Trigger_t = gsw::event_trigger<WindowLostFocus>;
@@ -102,7 +116,7 @@ public:
   EVENT_TYPE(EventType::WindowLostFocus)
 };
 
-class WindowMoved : public ApplicationEvent
+class WindowMoved : public WindowEvent
 {
 public:
   using Trigger_t = gsw::event_trigger<WindowMoved>;
