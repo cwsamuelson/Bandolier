77bdc070fbb95b708bd3c167858cdfb8c3889ace
Chris <chris.sam55@gmail.com>
2019-10-01 20:42:20 -0400

    Added windows input implementation

---
+++ Bandolier/code/platform/Windows/windows_input.cc
@@ -0,0 +1,37 @@
+#include <GLFW/glfw3.h>
+
+#include "Application.hh"
+
+#include "windows_input.hh"
+
+namespace Bandolier{
+
+Input* Input::Instance = new WindowsInput;
+
+bool
+WindowsInput::IsKeyPressedImpl(int keycode)
+{
+  auto window = static_cast<GLFWwindow*>(Application::Get().Window().Native());
+  auto state = glfwGetKey(window, keycode);
+  return state == GLFW_PRESS || state == GLFW_REPEAT;
+}
+
+bool
+WindowsInput::IsMouseButtonPressedImpl(int button)
+{
+  auto window = static_cast<GLFWwindow*>(Application::Get().Window().Native());
+  auto state = glfwGetMouseButton(window, button);
+  return state == GLFW_PRESS;
+}
+
+std::tuple<float, float>
+WindowsInput::GetMousePositionImpl()
+{
+  auto window = static_cast<GLFWwindow*>(Application::Get().Window().Native());
+  double X;
+  double Y;
+  glfwGetCursorPos(window, &X, &Y);
+  return {float(X), float(Y)};
+}
+
+}
---
+++ Bandolier/code/platform/Windows/windows_input.hh
@@ -0,0 +1,21 @@
+#ifndef BANDOLIER_WINDOWS_INPUT_HH
+#define BANDOLIER_WINDOWS_INPUT_HH
+
+#include <Input.hh>
+
+namespace Bandolier {
+
+class WindowsInput : public Input
+{
+protected:
+  virtual bool
+  IsKeyPressedImpl(int keycode) override;
+  virtual bool
+  IsMouseButtonPressedImpl(int button) override;
+  virtual std::tuple<float, float>
+  GetMousePositionImpl() override;
+};
+
+}
+
+#endif //BANDOLIER_WINDOWS_INPUT_HH
