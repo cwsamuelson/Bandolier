3112f36bd891873d5495cd7935f67d585fd7f04a
Chris <chris.sam55@gmail.com>
2019-10-01 20:42:10 -0400

    Added input base class

---
+++ Bandolier/code/include/Input.hh
@@ -0,0 +1,33 @@
+#ifndef BANDOLIER_INPUT_HH
+#define BANDOLIER_INPUT_HH
+
+#include <tuple>
+
+namespace Bandolier{
+
+class Input{
+public:
+  inline static
+  bool
+  IsKeyPressed(int keycode){ return Instance->IsKeyPressedImpl(); }
+  inline static
+  bool
+  IsMouseButtonPressed(int button){ return Instance->IsMouseButtonPressedImpl(button); }
+  inline static
+  std::tuple<float, float>
+  GetMousePosition(){ return Instance->GetMousePositionImpl(); }
+
+protected:
+  static Input* Instance;
+
+  virtual bool
+  IsKeyPressedImpl(int keycode) = 0;
+  virtual bool
+  IsMouseButtonPressedImpl(int button) = 0;
+  virtual std::tuple<float, float>
+  GetMousePositionImpl() = 0;
+};
+
+};
+
+#endif //BANDOLIER_INPUT_HH
