97cb597e61b80a99412b6bb62c241c3a750beef3
Chris <chris.sam55@gmail.com>
2019-10-06 16:28:27 -0400

    Informational logging

--- Bandolier/code/platform/OpenGL/open_gl_context.cc
+++ Bandolier/code/platform/OpenGL/open_gl_context.cc
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "logger.hh"
+
 #include "open_gl_context.hh"
 
 namespace Bandolier {
@@ -10,6 +12,8 @@ OpenGLContext::OpenGLContext(GLFWwindow* window)
 {
   glfwMakeContextCurrent(mWindow);//! @TODO validate mWindow
   int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);//! @TODO validLoading
+
+  logging::core()->info("OpenGL info:\n\tVendor: {0}\n\tRenderer: {1}\n\tVersion: {2}", glGetString(GL_VENDOR), glGetString(GL_RENDERER), glGetString(GL_VERSION));
 }
 
 OpenGLContext::~OpenGLContext()
