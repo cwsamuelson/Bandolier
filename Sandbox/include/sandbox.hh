50d50057e8818f566a409d58b5e35ebb618f41f4
Chris <chris.sam55@gmail.com>
2019-10-06 16:36:48 -0400

    Drawing a triangle!
    it's black on a dark grey background...but it's there!

--- Sandbox/include/sandbox.hh
+++ Sandbox/include/sandbox.hh
@@ -10,6 +10,11 @@ public:
   ~Sandbox() = default;
 
   void run() override;
+
+private:
+  unsigned int mVAO;
+  unsigned int mVBO;
+  unsigned int mIBO;
 };
 
 #endif //TEST_SANDBOX_HH
--- Sandbox/source/sandbox.cc
+++ Sandbox/source/sandbox.cc
@@ -28,17 +28,44 @@ Sandbox::Sandbox()
 
   //example layer?...crap!
   PushOverlay(std::make_shared<Bandolier::ImguiLayer>());
+
+  glGenVertexArrays(1, &mVAO);
+  glBindVertexArray(mVAO);
+
+  glGenBuffers(1, &mVBO);
+  glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+
+  glGenBuffers(1, &mIBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIBO);
+
+  float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f,
+  };
+  unsigned int indices[] = {
+          0, 1, 2,
+  };
+
+  glBufferData(GL_ARRAY_BUFFER,         sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
 }
 
 void Sandbox::run()
 {
-  glClearColor(1, 0, 1, 1);
+  glClearColor(0.1f, 0.1f, 0.1f, 1);
 
   mRunning = true;
   while(mRunning)
   {
     mWindow->OnUpdate();
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glBindVertexArray(mVAO);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
     for(auto& layer : mLayerStack)
     {
