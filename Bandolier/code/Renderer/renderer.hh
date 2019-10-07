63851bb1c5f2def5ae03d8179a7885fff1ff7d9d
Chris <chris.sam55@gmail.com>
2019-10-07 18:29:26 -0400

    Added initial renderer class with API specs

---
+++ Bandolier/code/Renderer/renderer.hh
@@ -0,0 +1,26 @@
+#ifndef BANDOLIER_RENDERER_HH
+#define BANDOLIER_RENDERER_HH
+
+namespace Bandolier{
+
+
+class Renderer
+{
+public:
+  enum class API{
+    None = 0,
+    OpenGL,
+    Direct3D,
+    Vulkan,
+    DirectX = int(Direct3D),
+  };
+
+  inline static API GetAPI(){ return sRendererAPI; }
+
+private:
+  inline static API sRendererAPI = API::OpenGL;
+};
+
+}
+
+#endif //BANDOLIER_RENDERER_HH
