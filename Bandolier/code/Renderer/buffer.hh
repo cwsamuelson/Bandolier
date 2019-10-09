193c38644b6657d0a624a77b4a162cb23787d634
Chris <chris.sam55@gmail.com>
2019-10-08 20:43:56 -0400

    Created buffer element

--- Bandolier/code/Renderer/buffer.hh
+++ Bandolier/code/Renderer/buffer.hh
@@ -67,6 +67,67 @@ ShaderDataTypeSize(ShaderDataType type)
   logging::core()->error("Unknown shader data type");//! @TODO throw?
   return 0;
 }
+
+struct BufferElement
+{
+  std::string Name;
+  ShaderDataType Type;
+  uint32_t Size;
+  uint32_t Offset;
+  bool Normalized;
+
+  BufferElement(ShaderDataType type, std::string name, bool normalized = false)
+    : Name(std::move(name))
+    , Type(type)
+    , Size(ShaderDataTypeSize(type))
+    , Offset(0)
+    , Normalized(normalized)
+  {}
+
+  uint32_t
+  GetComponentCount() const
+  {
+    switch(Type)
+    {
+    case ShaderDataType::Float:
+      return 1;
+
+    case ShaderDataType::Float2:
+      return 2;
+
+    case ShaderDataType::Float3:
+      return 3;
+
+    case ShaderDataType::Float4:
+      return 4;
+
+    case ShaderDataType::Mat3:
+      return 3 * 3;
+
+    case ShaderDataType::Mat4:
+      return 4 * 4;
+
+    case ShaderDataType::Int:
+      return 1;
+
+    case ShaderDataType::Int2:
+      return 2;
+
+    case ShaderDataType::Int3:
+      return 3;
+
+    case ShaderDataType::Int4:
+      return 4;
+
+    case ShaderDataType::Bool:
+      return 1;
+    }
+
+    logging::core()->error("Unjnown shader data type!");//! @TODO throw?
+    return 0;
+  }
+};
+
 class VertexBuffer
 {
 public:
