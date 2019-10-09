e8e353ebec86474c8c69c1c3b0748c5c29ee696d
Chris <chris.sam55@gmail.com>
2019-10-08 20:43:44 -0400

    Added shader data type size getter

--- Bandolier/code/Renderer/buffer.hh
+++ Bandolier/code/Renderer/buffer.hh
@@ -25,6 +25,48 @@ enum class ShaderDataType
   Int1 = Int,
 };
 
+static uint32_t
+ShaderDataTypeSize(ShaderDataType type)
+{
+  switch(type)
+  {
+  case ShaderDataType::Float:
+    return 4;
+
+  case ShaderDataType::Float2:
+    return 4 * 2;
+
+  case ShaderDataType::Float3:
+    return 4 * 3;
+
+  case ShaderDataType::Float4:
+    return 4 * 4;
+
+  case ShaderDataType::Mat3:
+    return 4 * 3 * 3;
+
+  case ShaderDataType::Mat4:
+    return 4 * 4 * 4;
+
+  case ShaderDataType::Int:
+    return 4;
+
+  case ShaderDataType::Int2:
+    return 4 * 2;
+
+  case ShaderDataType::Int3:
+    return 4 * 3;
+
+  case ShaderDataType::Int4:
+    return 4 * 4;
+
+  case ShaderDataType::Bool:
+    return 1;
+  }
+
+  logging::core()->error("Unknown shader data type");//! @TODO throw?
+  return 0;
+}
 class VertexBuffer
 {
 public:
