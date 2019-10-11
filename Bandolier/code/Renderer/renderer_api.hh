34f771fe7fe65c28ceec1c0eae383406fabd8933
Chris <chris.sam55@gmail.com>
2019-10-10 20:30:36 -0400

    Created renderer API interface

---
+++ Bandolier/code/Renderer/renderer_api.hh
@@ -0,0 +1,41 @@
+#ifndef BANDOLIER_RENDERER_API_HH
+#define BANDOLIER_RENDERER_API_HH
+
+#include <memory>
+
+//! @TODO fix this include path!
+#include "../../vendor/glm/glm/glm.hpp"
+#include "vertex_array.hh"
+
+namespace Bandolier {
+
+class RendererAPI
+{
+public:
+  enum class API{
+    None = 0,
+    OpenGL,
+    Direct3D,
+    Vulkan,
+    DirectX = int(Direct3D),
+  };
+
+  virtual void
+  SetClearColor(const glm::vec4& color) = 0;
+
+  virtual void
+  Clear() = 0;
+
+  virtual void
+  DrawIndexed(std::shared_ptr<VertexArray> VAO) = 0;
+
+  inline static API
+  GetAPI(){ return sRendererAPI; }
+
+private:
+  inline static API sRendererAPI = API::OpenGL;
+};
+
+}
+
+#endif //BANDOLIER_RENDERER_API_HH
