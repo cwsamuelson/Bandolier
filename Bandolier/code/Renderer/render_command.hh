7d07424e7fa98b4f1b1fbf0b5be626fd38886a69
Chris <chris.sam55@gmail.com>
2019-10-10 20:46:54 -0400

    abstracting render commands away

---
+++ Bandolier/code/Renderer/render_command.hh
@@ -0,0 +1,38 @@
+#ifndef BANDOLIER_RENDER_COMMAND_HH
+#define BANDOLIER_RENDER_COMMAND_HH
+
+#include <memory>
+
+#include "Renderer/renderer_api.hh"
+#include "platform/OpenGL/open_gl_renderer_api.hh"
+
+namespace Bandolier {
+
+class RenderCommand
+{
+public:
+  inline static void
+  SetClearColor(const glm::vec4& color)
+  {
+    sRendererAPI->SetClearColor(color);
+  }
+
+  inline static void
+  Clear()
+  {
+    sRendererAPI->Clear();
+  }
+
+  inline static void
+  DrawIndexed(std::shared_ptr<VertexArray> VAO)
+  {
+    sRendererAPI->DrawIndexed(VAO);
+  }
+
+private:
+  inline static std::unique_ptr<RendererAPI> sRendererAPI = std::make_unique<OpenGlRendererAPI>();
+};
+
+}
+
+#endif //BANDOLIER_RENDER_COMMAND_HH
