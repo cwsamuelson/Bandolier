c5627af25a98fbba2355a37d21592af84de4f631
Chris <chris.sam55@gmail.com>
2019-10-15 21:07:08 -0400

    using transform in our example shaders

--- Sandbox/source/sandbox.cc
+++ Sandbox/source/sandbox.cc
@@ -66,12 +66,13 @@ out vec3 v_Position;
 out vec4 v_Color;
 
 uniform mat4 u_ViewProjection;
+uniform mat4 u_Transform;
 
 void main()
 {
   v_Color = a_Color;
   v_Position = a_Position;
-  gl_Position = u_ViewProjection * vec4(a_Position, 1.0f);
+  gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0f);
 }
 )glsl";
 
@@ -97,11 +98,12 @@ layout(location = 0) in vec3 a_Position;
 out vec3 v_Position;
 
 uniform mat4 u_ViewProjection;
+uniform mat4 u_Transform;
 
 void main()
 {
   v_Position = a_Position;
-  gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+  gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 }
 )glsl";
 
