ce265aca3f14289f9212bc468c25497513933016
Chris <chris.sam55@gmail.com>
2019-10-22 21:00:03 -0400

    Created error messaging

---
+++ Bandolier/code/include/errors.hh
@@ -0,0 +1,49 @@
+#ifndef BANDOLIER_ERRORS_HH
+#define BANDOLIER_ERRORS_HH
+
+#include <stdexcept>
+#include <string>
+
+#include "logger.hh"
+
+namespace Bandolier::ErrorHandling{
+
+#define BNDLR_LOG_ERROR(message) ::Bandolier::logging::core()->error((message));
+#define BNDLR_LOG_WARN(message) ::Bandolier::logging::core()->warn((message));
+#define BNDLR_LOG_TRACE(message) ::Bandolier::logging::core()->warn((message));
+
+#define BNDLR_WRAPPER(stuff) do{stuff}while(false);
+#define BNDLR_CHECKER(condition, log, error) BNDLR_WRAPPER(if(!(condition)){log;error;})
+#define BNDLR_THROW(expression, message) throw ::Bandolier::ErrorHandling::Exception(#expression, message, __FILE__, __LINE__);
+
+#define BNDLR_ASSERT(expression, message) BNDLR_CHECKER(expression, BNDLR_LOG_ERROR(message), BNDLR_THROW(expression, message))
+#define BNDLR_CHECK(expression, message) BNDLR_CHECKER(expression, BNDLR_LOG_WARN(message), )
+#define BNDLR_CHECK_RV(expression, message, ret) BNDLR_CHECKER(expression, BNDLR_LOG_WARN(message), return ret;)
+#define BNDLR_TRACK(expression, message) BNDLR_CHECKER(expression, BNDLR_LOG_TRACE(message), )
+
+
+class Exception : public std::exception
+{
+private:
+  std::string mMessage;
+
+public:
+  Exception(const std::string& expression, const std::string& message, const std::string& fileName, unsigned int lineNumber);
+
+  [[nodiscard]]
+  const char*
+  what() const noexcept override
+  {
+    return mMessage.c_str();
+  }
+};
+
+Exception::Exception(const std::string& expression, const std::string& message, const std::string& fileName,
+                     unsigned int lineNumber)
+  : mMessage(message + " occurred when executing the expression: " + expression + " at " + fileName + ":" + std::to_string(lineNumber))
+{}
+
+}
+
+#endif //BANDOLIER_ERRORS_HH
+
