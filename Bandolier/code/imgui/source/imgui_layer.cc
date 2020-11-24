b24efd97ba4995849a130058a592f082272058ff
Chris <chris.sam55@gmail.com>
2020-11-24 15:54:12 -0500

    i think these files moved?

--- Bandolier/code/imgui/source/imgui_layer.cc
+++ Bandolier/code/imgui/source/imgui_layer.cc
@@ -3,8 +3,8 @@
 #include <imgui.h>
 
 #include <key_codes.hh>
-#include <examples/imgui_impl_glfw.h>
-#include <examples/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 #include "Application.hh"
 #include "logger.hh"
