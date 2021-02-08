4f8db39aed66c6458dbda86ebc522caaa2da36e1
Chris <chris.sam55@gmail.com>
2021-02-07 21:40:05 -0500

    update submodules, and fix associated linking errors

--- Bandolier/code/core/include/errors.hh
+++ Bandolier/code/core/include/errors.hh
@@ -14,11 +14,13 @@ namespace Bandolier::ErrorHandling {
 #define BNDLR_LOG_ERROR(...) ::Bandolier::logging::core()->error((__VA_ARGS__));
 #define BNDLR_LOG_WARN(...) ::Bandolier::logging::core()->warn((__VA_ARGS__));
 #define BNDLR_LOG_TRACE(...) ::Bandolier::logging::core()->trace((__VA_ARGS__));
+#define BNDLR_LOG_INFO(...) ::Bandolier::logging::core()->info((__VA_ARGS__));
 
 #define BNDLR_FAIL(...) GSW_WRAP(BNDLR_LOG_ERROR(__VA_ARGS__); GSW_THROW(__VA_ARGS__))
 #define BNDLR_ASSERT(expression, ...) GSW_VERIFY_AND(expression, BNDLR_LOG_ERROR(__VA_ARGS__), __VA_ARGS__)
 #define BNDLR_CHECK(expression, ...) GSW_CHECK_AND(expression, BNDLR_LOG_WARN(__VA_ARGS__), __VA_ARGS__)
 #define BNDLR_TRACK(expression, ...) GSW_CHECK_AND(expression, BNDLR_LOG_TRACE(__VA_ARGS__), __VA_ARGS__)
+#define BNDLR_INFORM(expression, ...) GSW_CHECK_AND(expression, BNDLR_LOG_INFO(__VA_ARGS__), __VA_ARGS__)
 
 #define VERIFY_AND_LOG(cond, ...) GSW_VERIFY_AND(cond, ::Bandolier::logging::core()->error(FMT format(__VA_ARGS__));, __VA_ARGS__);
 
